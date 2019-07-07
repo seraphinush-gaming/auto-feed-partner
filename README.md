4fcc96528e3cc9962e35abd2f11732f03f8da47c
seraph <seraphinush@gmail.com>
2019-07-08 01:58:01 +0900

    1.00.00

---
+++ README.md
@@ -0,0 +1,30 @@
+<p align="center">
+<a href="#">
+<img src="https://github.com/seraphinush-gaming/pastebin/blob/master/logo_ttb_trans.png?raw=true" width="200" height="200" alt="tera-toolbox, logo by Foglio" />
+</a>
+</p>
+
+# auto-qol [![paypal](https://img.shields.io/badge/paypal-donate-333333.svg?colorA=253B80&colorB=333333)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=B7QQJZV9L5P2J&source=url) [![paypal.me](https://img.shields.io/badge/paypal.me-donate-333333.svg?colorA=169BD7&colorB=333333)](https://www.paypal.me/seraphinush)
+tera-toolbox to automatically spawn companion upon login
+```
+Support seraph via paypal donations, thanks in advance !
+```
+
+## Auto-update guide
+- Create a folder called `auto-pet` in `tera-toolbox/mods` and download >> [`module.json`](https://raw.githubusercontent.com/seraphinush-gaming/auto-pet/master/module.json) << (right-click this link and save link as..) into the folder
+
+## Usage
+- __pet__
+  - Toggle on/off
+### Arguments
+- __set num__
+  - Set pet feeding interval to `num` minutes
+  - Default is `30` minutes
+
+## Changelog
+<details>
+
+    1.00
+    - Initial commit
+
+</details>
---
+++ index.js
@@ -0,0 +1,157 @@
+class AutoPet {
+
+  constructor(mod) {
+
+    this.mod = mod;
+    this.cmd = mod.command;
+    this.settings = mod.settings;
+    this.hooks = [];
+
+    this.food_interval = 0;
+    this.pet = undefined;
+    this.user = {
+      gameId: BigInt(0),
+      name: ''
+    }
+
+    // command
+    this.cmd.add('pet', {
+      '$none': () => {
+        this.settings.enable = !this.settings.enable;
+        this.send(`${this.settings.enable ? 'en' : 'dis'}abled`);
+      },
+      'set': (num) => {
+        if (!isNaN(num)) {
+          this.settings.interval = num;
+          this.send(`Set pet feeding interval to ${num} minutes.`);
+        } else {
+          this.send(`Invalid argument. usage : pet set (num)`);
+        }
+      },
+      '$default': () => {
+        this.send(`Invalid argument. usage : pet [set]`);
+      }
+    });
+
+    this.load();
+
+  }
+
+  destructor() {
+    this.mod.clearInterval(this.food_interval);
+    this.mod.saveSettings();
+    this.cmd.remove('pet');
+    this.unload();
+
+    this.user = undefined;
+    this.pet = undefined;
+    this.food_interval = undefined;
+
+    this.hooks = undefined;
+    this.settings = undefined;
+    this.cmd = undefined;
+    this.mod = undefined;
+  }
+
+  trySpawnPet() {
+    let pet = this.settings.pet[this.user.name];
+    this.mod.trySend('C_REQUEST_SPAWN_SERVANT', 1, {
+      id: pet.id,
+      dbid: BigInt(pet.dbid)
+    });
+  }
+
+  tryFeedingPet(id) {
+    try {
+      if (this.pet) {
+        this.mod.send('C_USE_ITEM', 3, {
+          gameId: this.user.gameId,
+          id: id,
+          amount: 1,
+          unk4: true
+        });
+        this.send(`Fed companion pet food.`);
+      } else {
+        this.mod.clearInterval(this.food_interval);
+      }
+    }
+    catch {
+      this.send(`Warning. pet food could not be fed.`);
+    }
+  }
+
+  // code
+  hook() {
+    this.hooks.push(this.mod.hook(...arguments));
+  }
+
+  load() {
+    this.hook('S_LOGIN', this.mod.majorPatchVersion >= 81 ? 13 : 12, { order: - 1000 }, (e) => {
+      this.user.gameId = e.gameId;
+      this.user.name = e.name;
+
+      this.mod.hookOnce('S_SPAWN_ME', 'raw', () => {
+        if (this.settings.enable) {
+          if (this.settings.pet[this.user.name]) {
+            this.trySpawnPet();
+            this.food_interval = this.mod.setInterval(() => {
+              this.tryFeedingPet(206049);
+            }, (this.settings.interval * 60 * 1000));
+            this.send(`Spawned companion.`);
+          }
+        }
+      });
+    });
+
+    this.hook('S_REQUEST_SPAWN_SERVANT', 2, { order: 10 }, (e) => {
+      if (e.ownerId === this.user.gameId) {
+        this.pet = e.gameId;
+        this.settings.pet[this.user.name] = { id: e.id, dbid: e.dbid.toString() };
+      }
+    });
+
+    this.hook('S_REQUEST_DESPAWN_SERVANT', 1, { order: 10 }, (e) => {
+      if (this.pet === e.gameId) {
+        this.pet = undefined;
+      }
+    });
+
+    this.hook('C_CAST_FISHING_ROD', 'raw', () => {
+      //
+      this.send('Fishing detected. despawning companion.');
+      try {
+        this.mod.send('C_REQUEST_DESPAWN_SERVANT', 1, {});
+      } catch {
+        this.send(`Warning. companion could not be despawned`);
+        this.mod.warn('Unmapped protocol packet \<C_REQUEST_DESPAWN_SERVANT\>.');
+      }
+    });
+  }
+
+  unload() {
+    if (this.hooks.length) {
+      for (let h of this.hooks)
+        this.mod.unhook(h);
+      this.hooks = [];
+    }
+  }
+
+  send() { this.cmd.message(': ' + [...arguments].join('\n\t - ')); }
+
+  // reload
+  saveState() {
+    let state = {
+      pet: this.pet,
+      user: this.user
+    }
+    return state;
+  }
+
+  loadState(state) {
+    this.pet = state.pet;
+    this.user = state.user;
+  }
+
+}
+
+module.exports = AutoPet;
